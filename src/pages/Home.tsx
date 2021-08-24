cde32dd46b772c58278b6093232ab4c05b885b7e
Roberto Fortes <robertbernd23@gmail.com>
2021-08-24 17:53:40 -0300

    chore: adding and listing tasks

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,13 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newData = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    };
+    
+    setTasks((oldTasks) => [...oldTasks, newData]);
   }
 
   function handleToggleTaskDone(id: number) {
